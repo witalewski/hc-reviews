66d87c953ba9e887460e87298dc87e243906ac17
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 12:50:59 +0100

    implement user reducer logic

--- src/state/reducers/userReducer.js
+++ src/state/reducers/userReducer.js
@@ -1,3 +1,10 @@
-export const user = (state = {}, action) => {
-  return state;
+import { RECEIVE_CURRENT_USER } from "../actions/userActions";
+
+export const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return action.currentUser;
+    default:
+      return state;
+  }
 };
