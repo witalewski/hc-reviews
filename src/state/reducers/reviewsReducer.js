b8f68d0e0341a8c436b9a2388c9365a3e0ab7202
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 11:13:58 +0100

    create receiveReviews action for initial setup

--- src/state/reducers/reviewsReducer.js
+++ src/state/reducers/reviewsReducer.js
@@ -1,38 +1,46 @@
 import mockReviews from "../mocks/reviews.json";
 import { SAVE_COMMENT } from "../actions/commentActions";
+import { RECEIVE_REVIEWS } from "../actions/reviewActions";
 
 const getNextId = (label, ids) =>
   `${label}${
     ids.length ? parseInt(ids[ids.length - 1].match(/\d+/)[0]) + 1 : 1
   }`;
 
-export const reviewsReducer = (state = mockReviews, action) => {
-  switch (action.type) {
-    case SAVE_COMMENT:
-      const commentId = getNextId("comment", state.comments.allIds);
-      return {
-        ...state,
-        items: {
-          ...state.items,
-          byId: {
-            ...state.items.byId,
-            [action.reviewId]: {
-              ...state.items.byId[action.reviewId],
-              comments: [
-                ...state.items.byId[action.reviewId].comments,
-                commentId
-              ]
-            }
-          }
-        },
-        comments: {
-          byId: {
-            ...state.comments.byId,
-            [commentId]: { id: commentId, ...action.comment }
-          },
-          allIds: [...state.comments.allIds, commentId]
+const addCommentToReview = (state, reviewId, comment) => {
+
+  const commentId = getNextId("comment", state.comments.allIds);
+  return {
+    ...state,
+    items: {
+      ...state.items,
+      byId: {
+        ...state.items.byId,
+        [reviewId]: {
+          ...state.items.byId[reviewId],
+          comments: [
+            ...state.items.byId[reviewId].comments,
+            commentId
+          ]
         }
-      };
+      }
+    },
+    comments: {
+      byId: {
+        ...state.comments.byId,
+        [commentId]: { id: commentId, ...comment }
+      },
+      allIds: [...state.comments.allIds, commentId]
+    }
+  }
+}
+
+export const reviewsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_REVIEWS:
+      return action.reviews;
+    case SAVE_COMMENT:
+      return addCommentToReview(state, action.reviewId, action.comment);
     default:
       return state;
   }
--- src/state/store.js
+++ src/state/store.js
@@ -1,13 +1,23 @@
 import { createStore, combineReducers } from "redux";
+import mockReviews from "./mocks/reviews.json";
+
+import { receiveReviews } from "./actions/reviewActions";
 import { reviewsReducer } from "./reducers/reviewsReducer";
 
 const reducers = {
   reviews: reviewsReducer
 };
 
-export const store = createStore(
-  combineReducers(reducers),
-  process.env.NODE_ENV === "development" &&
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+const initStore = store => {
+  store.dispatch(receiveReviews(mockReviews));
+  return store;
+};
+
+export const store = initStore(
+  createStore(
+    combineReducers(reducers),
+    process.env.NODE_ENV === "development" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
