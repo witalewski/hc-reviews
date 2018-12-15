80eeb4cf13d916e560092e3e2b7c2b35cca1c524
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 11:26:29 +0100

    add handling receiveReviews in ui state

--- src/state/reducers/uiReducer.js
+++ src/state/reducers/uiReducer.js
@@ -1,3 +1,13 @@
+import { RECEIVE_REVIEWS } from "../actions/reviewActions";
+
+const addReviewEntries = (state, reviewIds) =>
+  reviewIds.reduce((acc, id) => ({ ...acc, [id]: {} }), state);
+
 export const uiReducer = (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case RECEIVE_REVIEWS:
+      return addReviewEntries(state, action.reviews.items.allIds);
+    default:
+      return state;
+  }
 };
