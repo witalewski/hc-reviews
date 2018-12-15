8dfb183ce15888e46ac0a66fe8f9b3fb9b33d59f
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 07:05:01 +0100

    Updates

---
+++ precache-manifest.699174b0301ba7fe28faf90d0704e9a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hc-reviews/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229ef04cba27c2fc4750",
+    "url": "/hc-reviews/static/js/runtime~main.229ef04c.js"
+  },
+  {
+    "revision": "8faa86307d15def3b2a1",
+    "url": "/hc-reviews/static/js/main.8faa8630.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/hc-reviews/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "8faa86307d15def3b2a1",
+    "url": "/hc-reviews/static/css/main.b11ab253.chunk.css"
+  },
+  {
+    "revision": "723f1c8ebf01fcd66a91b03bc5d65cd6",
+    "url": "/hc-reviews/index.html"
+  }
+];
