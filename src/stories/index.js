8a523e266864471723ada8e22b4fe4c682c63d4d
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 10:55:38 +0100

    extract stories decorator to separate function

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { storiesOf, addDecorator } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 
 import { store } from "../state/store";
 
@@ -8,21 +8,17 @@ import mockData from "../state/mocks/reviews.json";
 import { ReviewConnected } from "../components/Review";
 import { Comment } from "../components/Comment";
 
-storiesOf("Review", module)
-  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
-  .add("Review 1", () => (
-    <ReviewConnected reviewId="review1" />
-  ))
-  .add("Review 2", () => (
-    <ReviewConnected reviewId="review2" />
-  ))
-  .add("Review 3", () => (
-    <ReviewConnected reviewId="review3" />
-  ))
-  .add("Review 4", () => (
-    <ReviewConnected reviewId="review4" />
-  ));
+const addProvider = story => <Provider store={store}>{story()}</Provider>;
 
-storiesOf("Comment", module).add("Comment 1", () => (
-  <Comment comments={mockData.comments.byId} commentId="comment1" />
-));
+storiesOf("Review", module)
+  .addDecorator(addProvider)
+  .add("Review 1", () => <ReviewConnected reviewId="review1" />)
+  .add("Review 2", () => <ReviewConnected reviewId="review2" />)
+  .add("Review 3", () => <ReviewConnected reviewId="review3" />)
+  .add("Review 4", () => <ReviewConnected reviewId="review4" />);
+
+storiesOf("Comment", module)
+  .addDecorator(addProvider)
+  .add("Comment 1", () => (
+    <Comment comments={mockData.comments.byId} commentId="comment1" />
+  ));
