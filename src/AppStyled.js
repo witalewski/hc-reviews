026e29d0f31c79a79a473d07bf9d4e5432cba51f
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 14:03:54 +0100

    add background

--- src/AppStyled.js
+++ src/AppStyled.js
@@ -1,4 +1,10 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
+
+import background from "./assets/background.jpg";
 
 export const AppStyled = styled.div`
+    min-height: 100vh;
+
+  background:  url(${background}) top center no-repeat, white;
+  background-size: 100vw;
 `;
