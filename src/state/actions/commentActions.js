6ad27639ee34de497f2b8abae6445ca3395ac9f0
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 12:26:19 +0100

    add action for adding comments

--- src/state/actions/commentActions.js
+++ src/state/actions/commentActions.js
@@ -1,7 +1,13 @@
 export const SAVE_COMMENT = "SAVE_COMMENT";
+export const ADD_COMMENT = "ADD_COMMENT";
 
 export const saveComment = (comment, reviewId) => ({
   type: SAVE_COMMENT,
   comment,
+  reviewId
+});
+
+export const addComment = reviewId => ({
+  type: ADD_COMMENT,
   reviewId
 });
