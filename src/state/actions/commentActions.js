f8399759e2dd953b713c6adb93a4ce40b91f2e68
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-15 12:37:41 +0100

    add cancel comment action

--- src/state/actions/commentActions.js
+++ src/state/actions/commentActions.js
@@ -1,5 +1,6 @@
 export const SAVE_COMMENT = "SAVE_COMMENT";
 export const ADD_COMMENT = "ADD_COMMENT";
+export const CANCEL_COMMENT = "CANCEL_COMMENT";
 
 export const saveComment = (comment, reviewId) => ({
   type: SAVE_COMMENT,
@@ -9,5 +10,10 @@ export const saveComment = (comment, reviewId) => ({
 
 export const addComment = reviewId => ({
   type: ADD_COMMENT,
+  reviewId
+});
+
+export const cancelComment = reviewId => ({
+  type: CANCEL_COMMENT,
   reviewId
 });
